dff8768247f931be21159b995538d5d912c0aa77
Jiffye Developers <devjiffye@gmail.com>
2022-03-09 08:40:07 +0100

    fixed a message on dashboard

--- resources/views/account/dashboard.blade.php
+++ resources/views/account/dashboard.blade.php
@@ -8,11 +8,17 @@
 	<div class="mb-sm-4 py-4 px-3 d-flex flex-wrap align-items-center text-head">
 		<h2 class="font-w600 mb-2 me-auto">Dashboard</h2>
 	</div>
+	<div class="container alert alert-info alert-block">
+        <button type="button" class="close" data-dismiss="alert">×</button> 
+        <strong class="text-center">Please ensure to keep your special login url safe, you can see ur special login link in your profile page</strong>
+    </div>
 	<div>
 		<h3 class="text-center">
 			Hi {{Auth::user()->name }} Welcome to Afrib Design Travel Stories</h3><br>
 		<p class="text-center">Become a travel writer today..!</p>
-	</div>	
+	</div>
+	
+	
 	<div class="d-flex justify-content-center align-items-center">
 		<div class="col-4">
 			<div class="card">
